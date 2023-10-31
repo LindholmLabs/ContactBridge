7f2ed9e5e7519afdf27834a688df0ec8362f97a1
William Lindholm <william_lindholm@outlook.com>
2023-11-01 00:10:22 +0100

    Added user authentication.

--- WebInterface/routes.py
+++ WebInterface/routes.py
@@ -1,12 +1,18 @@
-from flask import render_template
+import json
+from pathlib import Path
 
+from flask import Flask, request, redirect, url_for, render_template, session
 from  dbrepository import DatabaseRepository
 from . import web_interface
 
 db = DatabaseRepository()
 
+
 @web_interface.route('/home')
 def home():
+    if not session.get('logged_in'):
+        return redirect(url_for('web_interface.login'))
+
     messages = db.fetch_messages()
 
     formatted_messages = [
@@ -22,3 +28,28 @@ def home():
     ]
 
     return render_template('home.html', messages=formatted_messages)
+
+
+@web_interface.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        current_dir = Path(__file__).parent
+        settings_path = current_dir / '..' / 'settings.json'
+        with open(settings_path, 'r') as f:
+            settings = json.load(f)
+
+        if username == settings['username'] and password == settings['password']:
+            session['logged_in'] = True
+            return redirect(url_for('web_interface.home'))
+        else:
+            return "Invalid credentials", 401
+
+    return render_template('login.html')
+
+@web_interface.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('web_interface.login'))
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from WebAPI import blueprint as api_blueprint
 from WebInterface import web_interface
+import os
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/api')
 app.register_blueprint(web_interface)
+app.secret_key = os.urandom(24)
 
 if __name__ == '__main__':
     app.run(debug=True)
